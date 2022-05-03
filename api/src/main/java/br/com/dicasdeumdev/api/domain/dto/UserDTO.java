04e8b59ce82cab4baa0649cf0489be09ff11667e
jfcbx <jfcbx@DESKTOP-UNLKJN3>
2022-05-03 18:46:29 -0300

    retornando dto para user resource

--- api/src/main/java/br/com/dicasdeumdev/api/domain/dto/UserDTO.java
+++ api/src/main/java/br/com/dicasdeumdev/api/domain/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package br.com.dicasdeumdev.api.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,8 @@ public class UserDTO {
 	private Integer id;
 	private String name;
 	private String email;
+	
+	@JsonIgnore
 	private String password;
 
 }
--- api/src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ api/src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.api.resources;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.services.UserService;
 
 @RestController
@@ -15,11 +16,14 @@ import br.com.dicasdeumdev.api.services.UserService;
 public class UserResource {
 	
 	@Autowired
+	private ModelMapper mapper;
+		
+	@Autowired
 	private UserService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Integer id){
-		return ResponseEntity.ok().body(service.findById(id));
+	public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+		return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
 	}
 
 }
