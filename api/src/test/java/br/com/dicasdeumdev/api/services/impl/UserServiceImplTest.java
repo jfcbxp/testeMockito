148f6501eabaf7468e713769e57b4051972dc9ce
jfcbx <jfcbx@DESKTOP-UNLKJN3>
2022-05-04 10:20:39 -0300

    teste para classe user service metodo findById

---
+++ api/src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
@@ -0,0 +1,95 @@
+package br.com.dicasdeumdev.api.services.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
+import br.com.dicasdeumdev.api.repositories.UserRepository;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+	private static final String PASSWORD = "123";
+
+	private static final String EMAIL = "valdir@email.com";
+
+	private static final String NAME = "Valdir";
+
+	private static final Integer ID = 1;
+
+	@InjectMocks
+	private UserServiceImpl service;
+
+	@Mock
+	private UserRepository repository;
+
+	@Mock
+	private ModelMapper mapper;
+
+	private User user;
+
+	private UserDTO userDTO;
+
+	private Optional<User> optionalUser;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		startUser();
+	}
+
+	@Test
+	void whenFindByIdThenReturnAnUserInstance() {
+		when(repository.findById(anyInt())).thenReturn(optionalUser);
+
+		User response = service.findById(ID);
+
+		assertNotNull(response);
+		assertEquals(User.class, response.getClass());
+		assertEquals(ID, response.getId());
+		assertEquals(NAME, response.getName());
+		assertEquals(EMAIL, response.getEmail());
+
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	private void startUser() {
+		user = new User(ID, NAME, EMAIL, PASSWORD);
+		userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+		optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
+
+	}
+
+}
